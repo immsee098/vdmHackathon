b7bf7effe2732b85144a3795f60a23561b0ec73a
legendaryPark <pbjkmn00@gmail.com>
2024-05-12 19:39:35 -0700

    Created PostLikeView Request/Response/CustomRepository/CustomRepositoryImpl

---
+++ src/main/java/metlife/team01/application/adapter/in/controller/postShow/model/PostLikeViewRequest.java
@@ -0,0 +1,2 @@
+package metlife.team01.application.adapter.in.controller.postShow.model;public class PostLikeViewRequest {
+}
---
+++ src/main/java/metlife/team01/application/domain/postShow/repository/PostLikeViewCustomRepositoryImpl.java
@@ -0,0 +1,2 @@
+package metlife.team01.application.domain.postShow.repository;public class PostLikeViewRepositoryImpl {
+}
